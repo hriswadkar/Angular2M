250c75e43c141d92093fe0900477ccfce89eb3f9
Harshad <riswadkar.harshad@gmai.com>
2017-04-02 06:10:51 +0530

    Completed 1st assignment.
    Added warning and success alert components

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './successalert/successalert.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
